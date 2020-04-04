edfe9a0bb3a20ee2248b99aa48e317251266b688
张瀚文 <2016213259@bupt.edu.cn>
2020-04-05 00:05:33 +0800

    Complete GPU settings -- DEBUG3

--- models/mnist.py
+++ models/mnist.py
@@ -10,13 +10,6 @@ class Mnist(ModelGen):
     
     def __init__(self):
         super(Mnist, self).__init__()
-        self.model = None
-        self.loss_object = None
-        self.optimizer = None
-        self.train_loss = None
-        self.train_accuracy = None
-        self.test_loss = None
-        self.test_accuracy = None
     
     def train(self, prepared_data):
         
@@ -29,62 +22,59 @@ class Mnist(ModelGen):
         strategy = tf.distribute.MirroredStrategy()
         with strategy.scope():
             
-            self.model = MNISTModel()
-            self.loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
-            self.optimizer = tf.keras.optimizers.Adam()
-            self.train_loss = tf.keras.metrics.Mean(name='train_loss')
-            self.train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
-            self.test_loss = tf.keras.metrics.Mean(name='test_loss')
-            self.test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
+            model = MNISTModel()
+            loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
+            optimizer = tf.keras.optimizers.Adam()
+            train_loss = tf.keras.metrics.Mean(name='train_loss')
+            train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+            test_loss = tf.keras.metrics.Mean(name='test_loss')
+            test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
+
+            @tf.function
+            def train_step(xtf, ytl):
+                with tf.GradientTape() as tape:
+                    predictions = model(xtf)
+                    loss = loss_object(ytl, predictions)
+                gradients = tape.gradient(loss, model.trainable_variables)
+                optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    
+                train_loss(loss)
+                train_accuracy(ytl, predictions)
+
+            @tf.function
+            def test_step(xtf, ytl):
+                predictions = model(xtf)
+                t_loss = loss_object(xtf, predictions)
+    
+                test_loss(t_loss)
+                test_accuracy(ytl, predictions)
             
             epochs = 5
             for epoch in range(epochs):
+                
                 start = time.time()
-                self.train_loss.reset_states()
-                self.train_accuracy.reset_states()
-                self.test_loss.reset_states()
-                self.test_accuracy.reset_states()
+                train_loss.reset_states()
+                train_accuracy.reset_states()
+                test_loss.reset_states()
+                test_accuracy.reset_states()
                 
                 logger.info('Start training epoch %d' % (epoch + 1))
                 for (x_train_frame, y_train_label) in zip(x_train, y_train):
-                    self.train_step(x_train_frame, y_train_label)
+                    train_step(x_train_frame, y_train_label)
                 
                 logger.info('Start testing epoch %d' % (epoch + 1))
                 for (x_test_frame, y_test_label) in zip(x_test, y_test):
-                    self.test_step(x_test_frame, y_test_label)
+                    test_step(x_test_frame, y_test_label)
                 
                 end = time.time()
                 template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}, Take time: {} seconds.'
                 logger.info(template.format(epoch + 1,
-                            self.train_loss.result(),
-                            self.train_accuracy.result() * 100,
-                            self.test_loss.result(),
-                            self.test_accuracy.result() * 100),
+                            train_loss.result(),
+                            train_accuracy.result() * 100,
+                            test_loss.result(),
+                            test_accuracy.result() * 100),
                             (end - start)
                             )
-    
-    @tf.function
-    def train_step(self, x_train_frame, y_train_label):
-        strategy = tf.distribute.MirroredStrategy()
-        with strategy.scope():
-            with tf.GradientTape() as tape:
-                predictions = self.model(x_train_frame)
-                loss = self.loss_object(y_train_label, predictions)
-            gradients = tape.gradient(loss, self.model.trainable_variables)
-            self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
-            
-            self.train_loss(loss)
-            self.train_accuracy(y_train_label, predictions)
-    
-    @tf.function
-    def test_step(self, x_test_frame, y_test_label):
-        strategy = tf.distribute.MirroredStrategy()
-        with strategy.scope():
-            predictions = self.model(x_test_frame)
-            t_loss = self.loss_object(x_test_frame, predictions)
-            
-            self.test_loss(t_loss)
-            self.test_accuracy(y_test_label, predictions)
 
 
 class MNISTModel(tf.keras.Model):
