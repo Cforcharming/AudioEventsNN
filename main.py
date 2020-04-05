39f4d3f662aa646165aa4ab306d05bd413157197
张瀚文 <2016213259@bupt.edu.cn>
2020-04-05 23:02:58 +0800

    Reformat -- DEBUG8

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from dataset import mivia_db
 
 def main():
     
-    while True:
-        info = input('Use database and model (split by ' '):')
-        if info == 'exit':
-            break
-
-        training.train(info)
+    # while True:
+    #     info = input('Use database and model (split by ' '):')
+    #     if info == 'exit':
+    #         break
+    #
+    #     training.train(info)
     training.train('mnist mnist')
     # (x_train, y_train), (x_test, y_test) = mivia_db.load_data()
     # for i in x_train:
