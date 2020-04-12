cd5e08872c6115f4a2647d8065f9cd063599d5bd
张瀚文 <2016213259@bupt.edu.cn>
2020-04-12 18:51:52 +0800

    test for cnn

--- main.py
+++ main.py
@@ -24,9 +24,8 @@ def _construct_network(net):
         raise ValueError('Only cnn, bcnn, v3 or vgg are accepted as model.')
 
 
-def perform_train(ifs):
-    
-    info = ifs.split(' ')
+def perform_train():
+    info = infos.split(' ')
     logger.info('Preparing dataset %s and constructing model %s with epoch %d...' % (info[0], info[1], int(info[2])))
     
     gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -51,26 +50,24 @@ def perform_train(ifs):
                       shuffle=True,
                       callbacks=model.cbs
                       )
-            
+        
         except KeyboardInterrupt:
             logger.info('Stopped by KeyboardInterrupt.')
-            
+        
         except Exception as ex:
             logger.error(ex)
-            
+        
         finally:
             logger.info('Done training.')
 
 
-def perform_evaluate(ifs, db_level=None):
-    
-    info = ifs.split(' ')
+def perform_evaluate():
+    info = infos.split(' ')
     logger.info('Preparing dataset %s and testing model %s' % (info[0], info[1]))
     
-    tf.config.set_soft_device_placement(True)
     for gpu in tf.config.experimental.list_physical_devices('GPU'):
         tf.config.experimental.set_memory_growth(gpu, True)
-
+    
     strategy = tf.distribute.MirroredStrategy()
     with strategy.scope():
         
@@ -78,41 +75,61 @@ def perform_evaluate(ifs, db_level=None):
         model.compile(optimizer=model.optimizer_obj, loss=model.loss_obj, metrics=model.metrics_obj)
         model.build(input_shape=[32, 128, 128, 1])
         
-        latest = tf.train.latest_checkpoint('saved_params/%s/checkpoints/' % info[1])
-        if latest:
-            model.load_weights(latest)
-        else:
-            logger.error('No checkpoints found.')
-        train_ds, test_ds = _prepare_data(info[0], db_level)
-        try:
-            # TODO fix ValueError: Unknown Layer: CnnModel
-            loss, acc = model.evaluate(
-                x=test_ds,
-                verbose=1
-            )
-            if db_level is not None:
-                logger.info("Model %s accuracy on dataset %s for SNR=%d: %5.2f" % (info[1], info[0], db_level, acc))
-            else:
-                logger.info("Model %s accuracy on dataset %s: %5.2f" % (info[1], info[0], acc))
-        except ValueError as ex:
-            logger.error(ex)
+        latest = 'saved_params/cnn/checkpoints/0017_ckpt'
+        # latest = tf.train.latest_checkpoint('saved_params/%s/checkpoints/' % info[1])
+        model.load_weights(latest)
+        for db_level in range(5, 31, 5):
             
+            train_ds, test_ds = _prepare_data(info[0], db_level)
+            
+            loss, acc = model.evaluate(x=test_ds, verbose=1)
+            
+            logger.info("Model %s accuracy on dataset %s for SNR=%d: %f" % (info[1], info[0], db_level, acc))
+        logger.info('Done evaluating %s' % info[1])
 
-def t():
+
+def v():
+    logger.info('Preparing dataset mivia and constructing model v3 with epoch 25')
     
-    for infos in infos_list:
-        perform_train(infos)
-
-
-def e():
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
     
-    for infos in infos_list:
-        if infos.split(' ')[0] == 'mivia':
+    strategy = tf.distribute.MirroredStrategy()
+    with strategy.scope():
+        
+        train_ds, test_ds = _prepare_data('mivia')
+        
+        model = _construct_network('v3')
+        model.v3.compile(optimizer=model.optimizer_obj, loss=model.loss_obj, metrics=model.metrics_obj)
+        
+        try:
+            
+            model.v3.fit(x=train_ds,
+                         epochs=25,
+                         verbose=1,
+                         validation_data=test_ds,
+                         shuffle=True,
+                         callbacks=model.cbs
+                         )
+            
+            logger.info('Done training.')
+            
             for i in range(5, 31, 5):
                 logger.info('Evaluating performance on %ddB OF SNR' % i)
-                perform_evaluate(infos, db_level=i)
-        else:
-            perform_evaluate(infos)
+                train_ds, test_ds = _prepare_data('mivia', i)
+                loss, acc = model.evaluate(x=test_ds, verbose=1)
+                logger.info("Model %s accuracy on dataset %s for SNR=%d: %5.2f" % ('v3', 'mivia', i, acc))
+        
+        except KeyboardInterrupt:
+            logger.info('Stopped by KeyboardInterrupt.')
+        
+        except Exception as ex:
+            logger.error(ex)
+        
+        finally:
+            logger.info('Done v3.')
 
 
 if __name__ == '__main__':
@@ -121,6 +138,8 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format=fmt)
     logger = logging.getLogger('AudioEventsNN')
     
-    infos_list = ['mivia cnn 25', 'mivia bcnn 25', 'mivia vgg 25']
-    t()
-    # e()
+    infos = 'mivia cnn 25'
+    
+    # perform_train()
+    perform_evaluate()
+    # v()
